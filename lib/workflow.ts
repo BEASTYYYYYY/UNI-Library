7c938d647b3d1a4d728d082d1c51e81bfb8f056b
Yash kasera <yashkasera07@gmail.com>
2025-01-28 15:22:27 +0530

    fix email domain

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({
             provider: resend({ token: config.env.resendToken }),
         },
         body: {
-            from: "Yash Kasera <hello.uni-library.space>",
+            from: "Yash Kasera <contact@uni-library.space>",
             to: [email],
             subject,
             html: message,
