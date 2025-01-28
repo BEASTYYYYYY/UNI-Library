868c908fbc6c94d4a411f51ca1ed11c34aed2f48
Yash kasera <yashkasera07@gmail.com>
2025-01-28 14:51:37 +0530

    implement qstash workflows for user orboarding

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -3,12 +3,13 @@
 import { signIn } from "@/auth";
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
-import { hash } from "crypto";
 import bcrypt from "bcryptjs";
 import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import ratelimit from "../ratelimit";
 import { redirect } from "next/navigation";
+import { workflowClient } from "../workflow";
+import config from "../config";
 
 export const signInWithCredentials = async (
     params: Pick<AuthCredentials, "email" | "password">,
@@ -68,13 +69,13 @@ export const signUp = async (params: AuthCredentials) => {
             universityCard,
         });
 
-        // await workflowClient.trigger({
-        //     url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
-        //     body: {
-        //         email,
-        //         fullName,
-        //     },
-        // });
+        await workflowClient.trigger({
+            url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+            body: {
+                email,
+                fullName,
+            },
+        });
 
         await signInWithCredentials({ email, password });
 
